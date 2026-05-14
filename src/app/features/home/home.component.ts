4159c4db0123a31a5b0d97346b996867ac44b09e
David.nasc233 <david.nasc233@gmail.com>
2026-05-13 21:35:49 -0300

    feat(cart): Introduce cart items and payment components with styling and functionality

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
-import { NavBarComponent } from '../../shared/nav-bar/nav-bar.component';
-import { NavMenuComponent } from '../../shared/nav-menu/nav-menu.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { FoodMenu } from './food-menu/food-menu';
 import { DishCard } from './dish-card/dish-card';
 import { RestaurantSelector } from './restaurant-selector/restaurant-selector';
-import { ChooseRestaurantModal } from './restaurant-selector/choose-restaurant-modal/choose-restaurant-modal';
-import { LgpdModalComponent } from '../../shared/lgpd-modal/lgpd-modal';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, NavMenuComponent, CarouselComponent, FoodMenu, DishCard, RestaurantSelector, ChooseRestaurantModal],
+  imports: [CarouselComponent, FoodMenu, DishCard, RestaurantSelector],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-
 export class HomeComponent {
+  
 }
