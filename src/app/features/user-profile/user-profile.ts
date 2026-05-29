b507438af017d2999413fb46518795a8d7665866
David.nasc233 <david.nasc233@gmail.com>
2026-05-28 22:42:46 -0300

    feat(user-profile): Display user's order history with order details in user profile

--- src/app/features/user-profile/user-profile.ts
+++ src/app/features/user-profile/user-profile.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { ValidateLgpdService } from '../../services/validate-lgpd-consent.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { IUserProfile } from '../../models/user.model';
 import { UserStorageService } from '../../services/user-storage.service';
 import { Router } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { IOrder } from '../../models/order.model';
 
 @Component({
   selector: 'app-user-profile',
@@ -14,17 +16,21 @@ import { Router } from '@angular/router';
 })
 export class UserProfile {
   hasAcceptedLgpd$!: Observable<boolean | null>;
+  orders$!: Observable<IOrder[]>;
   user$!: Observable<IUserProfile | null>;
 
   constructor(
     private readonly validateLgpdService: ValidateLgpdService,
     private readonly userStorageService: UserStorageService,
+    private readonly cartService: CartService,
     private readonly router: Router,
   ) {}
 
   ngOnInit() {
     this.hasAcceptedLgpd$ = this.validateLgpdService.lgpdConsent$;
     this.user$ = this.userStorageService.user$;
+    this.orders$ = this.cartService.orders$;
+    console.log(this.orders$)
   }
 
   revokeLgpdConsent(): void {
